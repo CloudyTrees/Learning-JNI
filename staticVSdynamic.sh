0462111b637f3a859cab0126d0cedb343fb85022
Steve Huang <shuang@broadinstitute.org>
2016-06-20 10:30:34 -0400

    update

--- staticVSdynamic.sh
+++ staticVSdynamic.sh
@@ -2,10 +2,8 @@
 u1="argument          meaning                             possible values"
 u2="1                 generate static or dynamic lib      [s/d]"
 u3="2                 C implementation file"
-u4="3                 output library name w/o extension   \"lib\" automatically prefixed if dynamic"
+u4="3                 output library name w/o extension   \"lib\" automatically prefixed"
 u5="4                 path to jni.h"
-
-prefix=$(echo $2 | awk -F'.c' '{print $1}')
 
 if [ -z ${4+x} ]; then 
     jnilibpath=/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers
@@ -18,6 +16,7 @@ if [ "$1" == "d" ]; then
         -I $jnilibpath \
         -o lib$3.dylib
 elif [ "$1" == "s" ]; then
+    prefix=$(echo $2 | awk -F'.c' '{print $1}')
     gcc -c $2 \
         -fpic \
         -I $jnilibpath \
